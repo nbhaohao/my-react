317e6fc4b77d8ebbba98ae67512bffde28cd2a69
nbhaohao <nbhaohao@163.com>
2021-02-19 15:24:23 +0800

    feat:完成 fiber 新增、删除、更新

--- src/react/react-dom.js
+++ src/react/react-dom.js
@@ -1,16 +1,25 @@
-import { REACT_TEXT_NODE } from "./const";
+import {
+  REACT_EFFECT_DELETE_TAG,
+  REACT_EFFECT_PLACEMENT_TAG,
+  REACT_EFFECT_UPDATE_TAG,
+  REACT_TEXT_NODE,
+} from "./const";
 
 function createDom(fiber) {
   const dom =
     fiber.type === REACT_TEXT_NODE
       ? document.createTextNode("")
       : document.createElement(fiber.type);
-  const isProperty = (key) => key !== "children";
   Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => (dom[name] = fiber.props[name]));
   return dom;
 }
+
+const isEvent = (key) => key.startsWith("on");
+const isProperty = (key) => key !== "children" && !isEvent(key);
+const isNew = (prev, next) => (key) => prev[key] !== next[key];
+const isGone = (prev, next) => (key) => !(key in next);
 
 function render(element, container) {
   wipRoot = {
@@ -18,29 +27,52 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot,
   };
+  deletions = [];
   nextUnitOfWork = wipRoot;
 }
 
 let wipRoot = null;
+let currentRoot = null;
 let nextUnitOfWork = null;
+let deletions = null;
 
-// 创建节点，根据 children 创建 fibers, 获取下一个需要处理的节点
-function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
-  }
-  const elements = fiber.props.children;
+function reconcileChildren(fiber, elements) {
   let index = 0;
+  let oldFiber = fiber.alternate && fiber.alternate.child;
   let prevSibling = null;
-  while (index < elements.length) {
+  while (index < elements.length || oldFiber !== null) {
     const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
+    let newFiber = null;
+    const sameType = oldFiber && element && element.type === oldFiber.type;
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: fiber,
+        alternate: oldFiber,
+        effectTag: REACT_EFFECT_UPDATE_TAG,
+      };
+    }
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: fiber,
+        alternate: null,
+        effectTag: REACT_EFFECT_PLACEMENT_TAG,
+      };
+    }
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = REACT_EFFECT_DELETE_TAG;
+      deletions.push(oldFiber);
+    }
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
     if (index === 0) {
       fiber.child = newFiber;
     } else {
@@ -49,7 +81,15 @@ function performUnitOfWork(fiber) {
     prevSibling = newFiber;
     index++;
   }
+}
 
+// 创建节点，根据 children 创建 fibers, 获取下一个需要处理的节点
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
   if (fiber.child) {
     return fiber.child;
   }
@@ -62,18 +102,59 @@ function performUnitOfWork(fiber) {
   }
 }
 
+function updateDom(dom, prevProps, nextProps) {
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.removeEventListener(eventType, prevProps[name]);
+    });
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = "";
+    });
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      dom[name] = nextProps[name];
+    });
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach((name) => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, nextProps[name]);
+    });
+}
+
 function commitWork(fiber) {
   if (!fiber) {
     return;
   }
+
   const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
+  if (fiber.effectTag === REACT_EFFECT_PLACEMENT_TAG && fiber.dom !== null) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === REACT_EFFECT_DELETE_TAG) {
+    domParent.removeChild(fiber.dom);
+  } else if (
+    fiber.effectTag === REACT_EFFECT_UPDATE_TAG &&
+    fiber.dom !== null
+  ) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
 
 function commitRoot() {
+  deletions.forEach(commitWork);
   commitWork(wipRoot.child);
+  currentRoot = wipRoot;
   wipRoot = null;
 }
 
